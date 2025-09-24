2ea5d051b77cd0db09fb609f7bc303a84a900cf7
Pijai <pijaitdm@gmail.com>
2025-09-24 21:08:49 +0700

    Update

--- Pproject/src/core.cpp
+++ Pproject/src/core.cpp
@@ -1,6 +1,7 @@
 #include "headers.hpp"
 #include "util.hpp"
 #include "menu.hpp"
+
 void top_header(){
 	std::string  header = "[ " + waktu("hari") + " || " + "PJ Project Management Laporan" + " || " + waktu("tanggal") + " ]";
 	std::cout << warna(header, "") << std::endl<<std::endl;;
@@ -17,39 +18,42 @@ void  PROSES_UTAMA(){
 	menu_navigasi(-1);
 		
 		int batas_pilih = 5;
+		
 		int pilih;
 	do{
 		pilih = input_angka("Pilih : ");
-		if(pilih == 0){
-			batas_pilih --;
-			std::cout << warna("Apakah ingin keluar/exit ? Tekan Y/n : ", "option");
-			char exit;
-			std::cin >> exit;
-			if(exit == 'Y' || exit ==  'y'){
-			break;
+		
+			if(pilih == 0){
+				batas_pilih--;
+				std::cout << warna("Apakah ingin keluar/exit ? Tekan Y/n : ", "option");
+				char exit;
+				std::cin >> exit;
+					if(exit == 'Y' || exit ==  'y'){
+					break;}
+					else{continue;}
 			}
-			else{continue;}
-		}
+
 		switch(pilih){
 			case 1 :
+					{
 					menu_navigasi(pilih);
 					std::cout << warna("Ini menu 1 !!\n", "option");
 					break;
-
+					}
 			case 2 :
-				{
+					{
 					menu_navigasi(pilih);
 					std::cout << warna("Ini menu 1 !!\n", "option");
 					break;
-				}
+					}
 
 			default :
-				{
+					{
 					menu_navigasi(pilih);
 					std::cout << pilih << std::endl;
 					std::cout << warna("Pilihan tidak ada !\n", "error");
 					batas_pilih--;
-				}
+					}
 		}
 
 	}while(batas_pilih != 0);
